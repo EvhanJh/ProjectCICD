9ccfc8ec454040db143469a653efa3f7e5c16a29
Evhan Jh <evhanjhpro@outlook.com>
2023-01-04 15:09:47 +0100

    Add files

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('Hello World !')
+"""App de test Project CI CD"""
+
+def main(elem_x, elem_y):
+    """
+
+    :param elem_x: int
+    :param elem_y: int
+    :return: int
+    """
+    result_x = elem_x - elem_y
+    return result_x + 1
+
+
+main(5, 2)
