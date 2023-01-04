4abdadf641ffd426c4b324baaf5edde2b666b660
Evhan Jh <evhanjhpro@outlook.com>
2023-01-04 14:20:46 +0100

    add files

---
+++ main.py
@@ -0,0 +1,11 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def main():
+    # Use a breakpoint in the code line below to debug your script.
+    print('Hello World !')  # Press ⌘F8 to toggle the breakpoint.
+
+main()
